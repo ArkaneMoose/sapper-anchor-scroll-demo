9d68335d2b2f836bbe36b99e7e92f4e09335f7fb
Rishov Sarkar <ArkaneMoose@users.noreply.github.com>
2020-04-01 12:11:01 -0400

    Fix relative links

--- src/routes/index.svelte
+++ src/routes/index.svelte
@@ -4,7 +4,7 @@
 
 <h1>Sapper Anchor Scroll Demo</h1>
 
-<p>First, go to the <a href="/about">about tab</a> and observe the 200 paragraphs on that page, each with their own anchor.</p>
+<p>First, go to the <a href="about">about tab</a> and observe the 200 paragraphs on that page, each with their own anchor.</p>
 
 <p>Then, return to this page, and <a href="#bottom">click here to go to the bottom of the page.</a></p>
 
@@ -12,4 +12,4 @@
 <p>Filler text.</p>
 {/each}
 
-<p id="bottom">Welcome to the bottom of the page. Finally, <a href="/about#anchor-100">click here to go to <code>anchor-100</code> on the about page.</a></p>
+<p id="bottom">Welcome to the bottom of the page. Finally, <a href="about#anchor-100">click here to go to <code>anchor-100</code> on the about page.</a></p>
